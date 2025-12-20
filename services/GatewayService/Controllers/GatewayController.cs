d81373dbc58f42a96ca79711c01e81b66bbb94d3
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-12-20 03:04:58 +0300

    работает

--- services/GatewayService/Controllers/GatewayController.cs
+++ services/GatewayService/Controllers/GatewayController.cs
@@ -548,7 +548,8 @@ namespace GatewayService.Controllers
                                 User.FindFirst(ClaimTypes.NameIdentifier) ?? 
                                 User.FindFirst(JwtRegisteredClaimNames.Sub) ??
                                 User.FindFirst("email") ??
-                                User.FindFirst("upn");
+                                User.FindFirst("upn") ??
+                                User.FindFirst("sub");
     
             if (usernameClaim == null)
                 throw new UnauthorizedAccessException("User not found in token claims");
