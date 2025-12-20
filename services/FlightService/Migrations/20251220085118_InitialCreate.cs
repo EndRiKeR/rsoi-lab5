012476b1777aa36e1708de2283799baeda2f3d30
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-12-20 11:52:44 +0300

    add migrations back

---
+++ services/FlightService/Migrations/20251220085118_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FlightService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    City = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Country = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FlightNumber = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Price = table.Column<int>(type: "integer", nullable: false),
+                    FromAirportId = table.Column<int>(type: "integer", nullable: true),
+                    ToAirportId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_FromAirportId",
+                        column: x => x.FromAirportId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_ToAirportId",
+                        column: x => x.ToAirportId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Airports_City_Name",
+                table: "Airports",
+                columns: new[] { "City", "Name" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_DateTime",
+                table: "Flights",
+                column: "DateTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FlightNumber",
+                table: "Flights",
+                column: "FlightNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FromAirportId",
+                table: "Flights",
+                column: "FromAirportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_ToAirportId",
+                table: "Flights",
+                column: "ToAirportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
