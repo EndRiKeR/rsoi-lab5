28a8103a100dca0a51b161ef43d34ccc39649d81
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2026-05-21 21:50:48 +0300

    save before final rush

--- minikube-deploy/minikube-build.sh
+++ minikube-deploy/minikube-build.sh
@@ -7,12 +7,14 @@ docker build -t tickets-service:$TAG -f ./services/TicketsService/Dockerfile .
 docker build -t bonus-service:$TAG -f ./services/BonusService/Dockerfile .
 docker build -t flight-service:$TAG -f ./services/FlightService/Dockerfile .
 docker build -t identity-service:$TAG -f ./services/IdentityService/Dockerfile .
+docker build -t statistic-service:$TAG -f ./services/StatisticService/Dockerfile .
 
 minikube image load gateway-service:$TAG
 minikube image load tickets-service:$TAG
 minikube image load bonus-service:$TAG
 minikube image load flight-service:$TAG
 minikube image load identity-service:$TAG
+minikube image load statistic-service:$TAG
 
 kubectl create namespace test --dry-run=client -o yaml | kubectl apply -f -
 
@@ -24,6 +26,12 @@ kubectl wait -n test -l app.kubernetes.io/instance=postgres pod --for=condition=
 helm upgrade identity-service ./charts/services --install --namespace test \
   -f ./charts/services/service-values/identity-values.yaml \
   --set image.repository=identity-service \
+  --set image.tag=$TAG \
+  --set image.pullPolicy=IfNotPresent
+  
+helm upgrade statistic-service ./charts/services --install --namespace test \
+  -f ./charts/services/service-values/statistic-values.yaml \
+  --set image.repository=statistic-service \
   --set image.tag=$TAG \
   --set image.pullPolicy=IfNotPresent
 
@@ -50,5 +58,6 @@ kubectl wait -n test -l app.kubernetes.io/instance=bonus-service pod --for=condi
 kubectl wait -n test -l app.kubernetes.io/instance=flight-service pod --for=condition=Ready --timeout=180s
 kubectl wait -n test -l app.kubernetes.io/instance=tickets-service pod --for=condition=Ready --timeout=180s
 kubectl wait -n test -l app.kubernetes.io/instance=gateway-service pod --for=condition=Ready --timeout=180s
+kubectl wait -n test -l app.kubernetes.io/instance=statistic-service pod --for=condition=Ready --timeout=180s
 
 kubectl get pods -n test -w
--- minikube-deploy/minikube-full-restart.sh
+++ minikube-deploy/minikube-full-restart.sh
@@ -1,5 +1,7 @@
-minikube stop
-minikube delete --all
+#!/bin/bash
+
+#minikube stop
+#minikube delete --all
 
 minikube start --driver=docker --cpus=4 --memory=6144
 
